93fe265d33e3c677715a50439fdb2a94cf675c03
Luke Westby <lukewestby1@gmail.com>
2020-08-12 10:36:26 -0700

    load civicengine script in form component

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,13 +2,36 @@ import React from 'react'
 import { css } from 'styled-components'
 
 const Form: React.FC = () => {
+  const container = React.useRef<HTMLDivElement | null>(null)
+
+  React.useEffect(() => {
+    if (!container.current) return
+
+    const script = document.createElement('script')
+    script.src = 'https://app.sunrisemovement.civicengine.com/embed.js'
+    container.current.appendChild(script)
+
+    const style = document.createElement('link')
+    style.rel = 'stylesheet'
+    style.href = 'https://app.sunrisemovement.civicengine.com/embed.css'
+    container.current.appendChild(style)
+
+    window.dispatchEvent(new Event('load'))
+  }, [])
+
   return (
     <div
+      ref={container}
       css={css`
         padding-bottom: 2em;
       `}
     >
-      Imagine a form here...
+      <div
+        className="civicengine-address"
+        data-input-email="required"
+        data-input-first-name="required"
+        data-input-last-name="required"
+      />
     </div>
   )
 }
